102e617f9b337340219f2a89df2bd87bdc3f8141
victorelliott <78336692+victorelliott@users.noreply.github.com>
2021-03-29 01:06:23 -0400

    Add tower

---
+++ TowerSiegeGame/Assets/Scripts/TowerShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShoot : MonoBehaviour
+{
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        } 
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
