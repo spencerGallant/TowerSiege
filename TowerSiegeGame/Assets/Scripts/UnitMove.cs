6ade9a654e94c0c1a5480e70f11dd8188b77be93
victorelliott <78336692+victorelliott@users.noreply.github.com>
2021-04-05 00:03:50 -0400

    Added some pathing functionality

--- TowerSiegeGame/Assets/Scripts/UnitMove.cs
+++ TowerSiegeGame/Assets/Scripts/UnitMove.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class UnitMove : MonoBehaviour
 {
+    public float speed;
+    // object must know this automatically when spawning
+    public int numWaypoints;
+    public int spawnPointIndex;
+
+    private Vector2[] waypoints;
+    private int waypointIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        waypoints = new Vector2[numWaypoints];
+        for (int i = 0; i < numWaypoints; i++)
+        {
+            waypoints[i] = GameObject.Find("waypoints0/waypoint" + i).transform.position;
+        }
+        waypointIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (waypointIndex == numWaypoints) 
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Vector2 currWaypoint = waypoints[waypointIndex];
+        transform.position = Vector2.MoveTowards(transform.position, currWaypoint, speed * Time.deltaTime);
+        if (transform.position.x == currWaypoint.x && transform.position.y == currWaypoint.y)
+        {
+            waypointIndex++;
+        }
     }
 }
