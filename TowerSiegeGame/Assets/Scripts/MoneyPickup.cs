5910f3beb0de540825cb3989f4b73aa3c22ba19b
spencerGallant <spencer.gallant@gmail.com>
2021-04-23 16:34:21 -0400

    added coin animation and start over button

--- TowerSiegeGame/Assets/Scripts/MoneyPickup.cs
+++ TowerSiegeGame/Assets/Scripts/MoneyPickup.cs
@@ -9,23 +9,32 @@ public class MoneyPickup : MonoBehaviour
 	private GameObject gameController;
     public int bagCost = 150;
     public int coinCost = 25;
+    private bool captured = false;
+    private GameObject coinSymbol;
 
     // Start is called before the first frame update
     void Start()
     {
         gameController = GameObject.FindGameObjectWithTag("GameController");
+        coinSymbol = GameObject.FindGameObjectWithTag("MoneySymbol");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        if(captured) {
+            transform.position = Vector3.MoveTowards(transform.position, coinSymbol.transform.position, .8f);
+        }
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
     	if (collision.gameObject.tag == "Player")
         {
+            captured = true;
+        }
+        else if(collision.gameObject.tag == "MoneySymbol"){
             int reducedPrice = bagCost;
             if(isCoin) reducedPrice = coinCost;
             gameController.GetComponent<Money>().CollectMoney(reducedPrice);
